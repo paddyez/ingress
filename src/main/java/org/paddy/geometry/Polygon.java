510b3eb5d4df2e72acae8b48eaf5906905f92c65
Patrick-Emil Zörner <paddyez@gmail.com>
2021-04-12 20:36:19 +0200

    buggy version

--- src/main/java/org/paddy/geometry/Polygon.java
+++ src/main/java/org/paddy/geometry/Polygon.java
@@ -9,32 +9,44 @@ import org.paddy.rest.dta.Point;
 
 public class Polygon extends Shape {
     private final DrawObject drawObject;
-    private final List<Double[]> allCartesianCoordinates = new ArrayList<>();
+    private final List<Double[]> allCoordinates = new ArrayList<>();
 
     public Polygon(DrawObject drawObject) {
         this.drawObject = drawObject;
         // System.out.println("Polygon");
         for (Point point : drawObject.getLatLngs()) {
             // System.out.println(point.getLat() + " " + point.getLng());
-            final Double[] cartesianCoordinates = new Double[3];
-            cartesianCoordinates[0] = Math.cos(DegGradRad.degToRad(point.getLat()))
-                    * Math.cos(DegGradRad.degToRad(point.getLng()));
-            cartesianCoordinates[1] = Math.cos(DegGradRad.degToRad(point.getLat()))
-                    * Math.sin(DegGradRad.degToRad(point.getLng()));
-            cartesianCoordinates[2] = Math.sin(DegGradRad.degToRad(point.getLat()));
-            allCartesianCoordinates.add(cartesianCoordinates);
+            final Double[] coordinates = new Double[2];
+            coordinates[0] = DegGradRad.degToRad(point.getLng());
+            coordinates[1] = DegGradRad.degToRad(point.getLat());
+            allCoordinates.add(coordinates);
         }
     }
 
     public BigDecimal[] getAngles() {
-        int size = allCartesianCoordinates.size();
+        int size = allCoordinates.size();
         final BigDecimal[] scalarProductAngles = new BigDecimal[size];
         for (int i = 0; i < size; i++) {
             BigDecimal scalarProductAngle = new BigDecimal(0);
-            Double[] a = allCartesianCoordinates.get(i);
-            Double[] b = allCartesianCoordinates.get((i + 1) % size);
-            Double[] c = allCartesianCoordinates.get((i + 2) % size);
-            BigDecimal scalarProduct = new BigDecimal(((b[0] - a[0]) * (c[0] - a[0]) + (b[1] - a[1]) * (c[1] - a[1]) + (b[2] - a[2]) * (c[2] - a[2])) / 2);
+            Double[] a = allCoordinates.get(i);
+            Double[] b = allCoordinates.get((i + 1) % size);
+            Double[] c = allCoordinates.get((i + 2) % size);
+            Double one   = (Math.cos(a[0]))*(Math.cos(a[1]))*(b[0] - a[0]);
+            Double two   = (Math.sin(a[0]))*(Math.sin(a[1]))*(b[1] - a[1]);
+            Double three = (Math.cos(a[0]))*(Math.cos(a[1]))*(c[0] - a[0]);
+            Double four  = (Math.sin(a[0]))*(Math.sin(a[1]))*(c[1] - a[1]);
+            final Double x =  (one + two) * (three + four);
+            one   = (Math.cos(a[0]))*(Math.sin(a[1]))*(b[0] - a[0]);
+            two   = (Math.sin(a[0]))*(Math.cos(a[1]))*(b[1] - a[1]);
+            three = (Math.cos(a[0]))*(Math.sin(a[1]))*(c[0] - a[0]);
+            four  = (Math.sin(a[0]))*(Math.cos(a[1]))*(c[1] - a[1]);
+            final Double y =  (one + two) * (three + four);
+            final Double z = (-Math.sin(a[0])*(b[0] - a[0])) * (-Math.sin(a[0])*(c[0] - a[0]));
+            BigDecimal scalarProduct = new BigDecimal(x + y + z / 2);
+            if(scalarProduct.doubleValue() > 1) {
+                System.out.println("Resetting scalar: " + scalarProduct.doubleValue());
+                scalarProduct = new BigDecimal(0);
+            }
             System.out.println("Scalar product: " + scalarProduct.doubleValue());
             scalarProductAngle = new BigDecimal(Math.acos(scalarProduct.doubleValue()));
             System.out.println("Angle: " + scalarProductAngle.doubleValue());
